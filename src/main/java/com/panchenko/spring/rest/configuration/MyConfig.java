e83bf262f50b959ad851a26ace66326bc9318bc9
Ranigale <panchenkonikita0@gmail.com>
2024-08-04 18:28:45 +0300

    create LocalSessionFactoryBean method

--- src/main/java/com/panchenko/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/panchenko/spring/rest/configuration/MyConfig.java
@@ -4,11 +4,13 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 import java.beans.PropertyVetoException;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.panchenko.spring.rest")
@@ -27,5 +29,20 @@ public class MyConfig {
             throw new RuntimeException(e);
         }
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.panchenko.spring.rest.entity");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        hibernateProperties.setProperty("show_sql", "true");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
     }
 }
