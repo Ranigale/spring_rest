25f4d930072faa2ec1e345fcf1332ae4dc85ed58
Ranigale <panchenkonikita0@gmail.com>
2024-08-05 19:08:49 +0300

    Added dao, entity, service

---
+++ src/main/java/com/panchenko/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.panchenko.spring.rest.dao;
+
+import com.panchenko.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
